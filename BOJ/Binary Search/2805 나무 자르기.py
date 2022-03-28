5414246fa945b6338790e6e8aafdb19db0d59411
pse0418 <43838044+pse0418@users.noreply.github.com>
2022-03-28 11:41:27 +0900

    Binary Search / Silver3
    2805 나무 자르기
    Binary Search / Silver3

---
+++ BOJ/Binary Search/2805 나무 자르기.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+h = list(map(int,input().split()))
+
+right = max(h)
+left = 1
+
+while(left <= right):
+    mid = (left + right) // 2
+    get_trees = 0
+    for i in h:
+        if(i > mid):
+            get_trees += (i - mid)
+    if(get_trees >= m):
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(right)
